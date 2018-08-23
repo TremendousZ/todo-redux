d332327f0fed8a8cfffd4084479a8f43d299b2b0
John Carlisle <jmcarlisle@live.com>
2018-08-23 12:58:12 -0700

    in progress

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,4 @@
 import types from './types';
 
+const API_KEY = '?key=c618_jCarlisle';
+const ScottsKey = "?key=c618_demouser";
---
+++ src/components/list.js
@@ -0,0 +1,14 @@
+import React ,{ Component } from 'react';
+
+class List extends Component{
+
+    render(){
+        return (
+            <ul className ='collection'>
+                <li className = 'collection-item'> This will be the todo list</li>
+            </ul>
+        )
+    }
+}
+
+export default List;
