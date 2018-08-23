25148f641a78299f91f20f69e58673525587dd00
John Carlisle <jmcarlisle@live.com>
2018-08-23 15:52:22 -0700

    pulling data from server and displaying to user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,17 @@
 import types from './types';
+import axios from 'axios';
 
+
+const BASE_URL= 'http://api.reactprototypes.com';
 const API_KEY = '?key=c618_jCarlisle';
 const ScottsKey = "?key=c618_demouser";
+
+export function get_all_list_data(){
+    const resp = axios.get(`${BASE_URL}/todos${API_KEY}`);
+
+    return {
+        type:types.GET_ALL_LIST_DATA,
+        payload: resp
+
+    };
+}
--- src/components/list.js
+++ src/components/list.js
@@ -1,14 +1,29 @@
 import React ,{ Component } from 'react';
+import { connect } from 'react-redux';
+import { get_all_list_data } from '../actions';
 
 class List extends Component{
+    componentDidMount(){
+        this.props.get_all_list_data();
+    }
+ 
 
     render(){
+        const listElements = this.props.list.map(item => {
+            return <li key={item._id} className = 'collection-item' >{item.title}</li>
+        });
         return (
             <ul className ='collection'>
-                <li className = 'collection-item'> This will be the todo list</li>
+                {listElements}
             </ul>
         )
     }
 }
 
-export default List;
+ function mapStateToProps( state ){
+    return {
+        list: state.list.all
+    }
+ }
+
+export default connect(mapStateToProps, {get_all_list_data:get_all_list_data})(List);
