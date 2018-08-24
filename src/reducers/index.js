bf84dd35f93e3864539cce05bc3b4a0b01f56818
John Carlisle <jmcarlisle@live.com>
2018-08-23 18:16:41 -0700

    finished redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import {combineReducers} from 'redux';
 import listReducer from './list_reducer';
+import { reducer as formReducer } from 'redux-form';
+
 
 const rootReducer = combineReducers({
-    list: listReducer
+    list: listReducer,
+    form: formReducer,
 });
 
 export default rootReducer;
