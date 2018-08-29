0451af2626de19b28233a602e1e40e223326814e
John Carlisle <jmcarlisle@live.com>
2018-08-29 16:42:30 -0700

    finsihed redux middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,6 +15,22 @@ export function get_all_list_data(){
     };
 }
 
+export const getSingleItem = (id) => async dispatch=>{
+    try{
+        const resp = await axios.get(`${BASE_URL}/todos/${id + API_KEY}`)
+        dispatch({
+            type: types.GET_SINGLE_ITEM,
+            payload: resp,
+        });
+    } catch(err){
+        dispatch({
+            type: types.LIST_ERROR,
+            error: 'No item found',
+        });
+    }
+
+}
+
 export function add_item(item){
     const resp = axios.post(`${BASE_URL}/todos${API_KEY}`, item);
 
@@ -23,4 +39,30 @@ export function add_item(item){
         payload: resp
     };
 
+}
+
+export function delete_item(id){
+    return async function (dispatch){
+        try{
+            const resp = await axios.delete(`${BASE_URL}/todos/${id + API_KEY}`)
+            console.log("delete worked");
+            dispatch({
+                type: types.DELETE_ITEM,
+                payload: resp
+            });
+        }catch (err){
+            console.log('delete failed');
+            dispatch({
+                type: types.LIST_ERROR,
+                error: 'Failed to delete item',
+            });
+        }
+        
+    }
+}
+
+export function reset_single(){
+    return{
+        type: types.RESET_SINGLE_VIEW,
+    };
 }
